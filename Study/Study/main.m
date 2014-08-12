7a061d3427d49b542a472bf1a220020d9cb6b8d7
funnyData <njuchendong@126.com>
2014-08-12 15:32:36 +0800

    Create a new project

---
+++ Study/Study/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Study
+//
+//  Created by mac on 14-8-12.
+//  Copyright (c) 2014年 NiuStock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
